48c22187a11a360dbfec258c2cfac33415878161
Stefan (nepo) <StNepomuk@web.de>
2012-05-06 16:04:36 +0200

    used MediaType constants for mime type settings

---
+++ PizzaAppApi/src/edu/hm/lip/pizza/api/communication/request/IDriverServiceLocal.java
@@ -0,0 +1,97 @@
+package edu.hm.lip.pizza.api.communication.request;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import edu.hm.lip.pizza.api.object.resources.Driver;
+import edu.hm.lip.pizza.api.object.resources.Order;
+
+/**
+ * REST-Service für die Fahrerdomäne. Verfügbare Aktionen: GET, POST, PUT, DELETE
+ * 
+ * @author Franz Mathauser
+ */
+@Local
+@Path( "/drivers" )
+@Produces( { MediaType.APPLICATION_JSON, MediaType.APPLICATION_ATOM_XML, MediaType.TEXT_XML } )
+@Consumes( { MediaType.APPLICATION_JSON, MediaType.APPLICATION_ATOM_XML, MediaType.TEXT_XML } )
+public interface IDriverServiceLocal
+{
+
+	/**
+	 * Liste aller Fahrer.
+	 * 
+	 * @return Fahrerliste
+	 */
+	@GET
+	@Path( "" )
+	List<Driver> findAll();
+
+	/**
+	 * Erzeugt ein neuen Fahrer.
+	 * 
+	 * @param driver
+	 *            Fahrer
+	 * @return erzeugten Fahrer
+	 */
+	@POST
+	@Path( "" )
+	Driver create( Driver driver );
+
+	/**
+	 * Liefere Fahrer anhand von id.
+	 * 
+	 * @param id
+	 *            Fahreridentifikation
+	 * @return Fahrer
+	 */
+	@GET
+	@Path( "{id}" )
+	Driver find( @PathParam( "id" ) int id );
+
+	/**
+	 * Liste der auszuliefernden Bestellungen in optimierter Reihenfolge für einen Fahrer mit id.
+	 * 
+	 * @param id
+	 *            Fahreridentifikation
+	 * @return Bestellungsliste
+	 */
+	@GET
+	@Path( "{id}/route" )
+	@QueryParam( "" )
+	List<Order> getRoute( @PathParam( "id" ) int id );
+
+	/**
+	 * Fügt eine Bestellungen einem Fahrer hinzu.
+	 * 
+	 * @param id
+	 *            Fahreridentifikation
+	 * @return Bestellungsliste
+	 */
+	@POST
+	@Path( "{id}/orders" )
+	List<Order> getOrders( @PathParam( "id" ) int id );
+
+	/**
+	 * Entferne zugeordnete Bestellung von Fahrer.
+	 * 
+	 * @param dId
+	 *            Fahreridentifikation
+	 * @param oId
+	 *            Bestellungsidentifikation
+	 */
+	@DELETE
+	@Path( "{dId}/orders/{oId}" )
+	void remove( @PathParam( "dId" ) int dId, @PathParam( "oId" ) int oId );
+
+}
---
+++ PizzaAppApi/src/edu/hm/lip/pizza/api/communication/request/IProductServiceLocal.java
@@ -0,0 +1,82 @@
+package edu.hm.lip.pizza.api.communication.request;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import edu.hm.lip.pizza.api.object.resources.Product;
+
+/**
+ * REST-Service für die Produktdomäne. Verfügbare Aktionen: GET, POST, PUT, DELETE
+ * 
+ * @author Franz Mathauser
+ */
+@Local
+@Path( "/products" )
+@Produces( { MediaType.APPLICATION_JSON, MediaType.APPLICATION_ATOM_XML, MediaType.TEXT_XML } )
+@Consumes( { MediaType.APPLICATION_JSON, MediaType.APPLICATION_ATOM_XML, MediaType.TEXT_XML } )
+public interface IProductServiceLocal
+{
+
+	/**
+	 * Liste aller Produkte.
+	 * 
+	 * @return Produktliste
+	 */
+	@GET
+	@Path( "" )
+	List<Product> findAll();
+
+	/**
+	 * Erzeugt ein neues Produkt.
+	 * 
+	 * @param product
+	 *            Produkt
+	 * @return erzeugte Produkt
+	 */
+	@POST
+	@Path( "" )
+	Product create( Product product );
+
+	/**
+	 * Liefere Product anhand von id.
+	 * 
+	 * @param id
+	 *            Produktidentifikator
+	 * @return Produkt
+	 */
+	@GET
+	@Path( "{id}" )
+	Product find( @PathParam( "id" ) int id );
+
+	/**
+	 * Überschreibe Produktdaten.
+	 * 
+	 * @param product
+	 *            Produkt
+	 * @return überschriebenes Produkt
+	 */
+	@PUT
+	@Path( "{id}" )
+	Product update( Product product );
+
+	/**
+	 * Lösche Produkt anhand von id.
+	 * 
+	 * @param id
+	 *            Produktidentifikator
+	 */
+	@DELETE
+	@Path( "{id}" )
+	void remove( @PathParam( "id" ) int id );
+
+}
