2150eb91cefea7badefb3cb70806acb53ac8c5cf
Franz Mathauser <franz.mathauser@googlemail.com>
2012-05-15 23:24:27 +0200

    REST-WS of driver list
    GET {base_url}/drivers
    
    #20

--- PizzaAppInternal/src/edu/hm/lip/pizza/internal/converter/DriverConverter.java
+++ PizzaAppInternal/src/edu/hm/lip/pizza/internal/converter/DriverConverter.java
@@ -1,7 +1,12 @@
 package edu.hm.lip.pizza.internal.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.hm.lip.pizza.api.object.resources.Driver;
+import edu.hm.lip.pizza.api.object.resources.OrderLine;
 import edu.hm.lip.pizza.internal.object.entities.EntityDriver;
+import edu.hm.lip.pizza.internal.object.entities.EntityOrderLine;
 
 /**
  * Konverter-Klasse fuer Driver-Objekte, um zwischen dem Service-Datenmodell und dem Entitaeten-Datenmodell zu
@@ -26,8 +31,10 @@ public final class DriverConverter
 	 */
 	public static Driver convertEntityToServiceDriver( EntityDriver eDriver )
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Driver driver = new Driver();
+		driver.setId( eDriver.getId() );
+		driver.setName( eDriver.getName() );
+		return driver;
 	}
 
 	/**
@@ -39,8 +46,34 @@ public final class DriverConverter
 	 */
 	public static EntityDriver convertServiceToEntityDriver( Driver driver )
 	{
-		// TODO Auto-generated method stub
-		return null;
+		EntityDriver eDriver = new EntityDriver();
+		eDriver.setId( driver.getId() );
+		eDriver.setName( driver.getName() );
+		return eDriver;
+	}
+
+	/**
+	 * Konvertiert eine Liste mit Fahrern aus dem Entity-Datenmodell in eine Liste mit Service-Modell.
+	 * 
+	 * @param eDriverList
+	 *            List mit Fahrern aus dem Entity-Modell
+	 * @return Liste mit Fahrern aus dem Service-Modell
+	 */
+	public static List<Driver> convertEntityToServiceDriverList( List<EntityDriver> eDriverList )
+	{
+		if (eDriverList == null || eDriverList.isEmpty())
+		{
+			return null;
+		}
+
+		List<Driver> driverList = new ArrayList<Driver>();
+
+		for (EntityDriver eDriver : eDriverList)
+		{
+			driverList.add( convertEntityToServiceDriver( eDriver ) );
+		}
+
+		return driverList;
 	}
 
 }
---
+++ PizzaAppServices/src/edu/hm/lip/pizza/services/bean/DriverService.java
@@ -0,0 +1,96 @@
+package edu.hm.lip.pizza.services.bean;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import edu.hm.lip.pizza.api.communication.request.IDriverServiceLocal;
+import edu.hm.lip.pizza.api.object.resources.Driver;
+import edu.hm.lip.pizza.api.object.resources.Order;
+import edu.hm.lip.pizza.internal.bean.database.IDriverDAOLocal;
+import edu.hm.lip.pizza.internal.converter.DriverConverter;
+
+
+/**
+ * @author Franz Mathauser, Stefan Wörner
+ *
+ */
+@Stateless
+public class DriverService implements IDriverServiceLocal
+{
+	@EJB
+	IDriverDAOLocal driverDAO;
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see edu.hm.lip.pizza.api.communication.request.IDriverServiceLocal#findAll()
+	 */
+	@Override
+	public List<Driver> findAll()
+	{
+		return DriverConverter.convertEntityToServiceDriverList( driverDAO.readAll() );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see edu.hm.lip.pizza.api.communication.request.IDriverServiceLocal#create(edu.hm.lip.pizza.api.object.resources.Driver)
+	 */
+	@Override
+	public Driver create( Driver driver )
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see edu.hm.lip.pizza.api.communication.request.IDriverServiceLocal#find(int)
+	 */
+	@Override
+	public Driver find( int id )
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see edu.hm.lip.pizza.api.communication.request.IDriverServiceLocal#getRoute(int)
+	 */
+	@Override
+	public List<Order> getRoute( int id )
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see edu.hm.lip.pizza.api.communication.request.IDriverServiceLocal#getOrders(int)
+	 */
+	@Override
+	public List<Order> getOrders( int id )
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see edu.hm.lip.pizza.api.communication.request.IDriverServiceLocal#remove(int, int)
+	 */
+	@Override
+	public void remove( int dId, int oId )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
