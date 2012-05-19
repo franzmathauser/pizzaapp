e4afe66d084c10667d19bbb0438895bed424ef47
Stefan (nepo) <StNepomuk@web.de>
2012-05-19 20:29:34 +0200

    added javadoc
    #30

--- PizzaAppDriver/src/edu/hm/lip/pizza/driver/exceptions/HostnameNotSetException.java
+++ PizzaAppDriver/src/edu/hm/lip/pizza/driver/exceptions/HostnameNotSetException.java
@@ -1,6 +1,8 @@
 package edu.hm.lip.pizza.driver.exceptions;
 
 /**
+ * Exception zeigt an, dass der Hostname für die Serververbindung nicht gesetzt ist.
+ * 
  * @author Stefan Wörner
  */
 public class HostnameNotSetException extends RuntimeException
@@ -8,11 +10,20 @@ public class HostnameNotSetException extends RuntimeException
 
 	private static final long serialVersionUID = 5536311157691373752L;
 
+	/**
+	 * Konstruktor.
+	 */
 	public HostnameNotSetException()
 	{
 
 	}
 
+	/**
+	 * Konstruktor.
+	 * 
+	 * @param message
+	 *            Fehlermeldung
+	 */
 	public HostnameNotSetException( String message )
 	{
 		super( message );
--- PizzaAppDriver/src/edu/hm/lip/pizza/driver/exceptions/HttpStatusCodeException.java
+++ PizzaAppDriver/src/edu/hm/lip/pizza/driver/exceptions/HttpStatusCodeException.java
@@ -1,6 +1,8 @@
 package edu.hm.lip.pizza.driver.exceptions;
 
 /**
+ * Exception zeigt an, dass ein HTTP Status Code ab 400 aufwärts empfangen wurde (HTTP Fehler).
+ * 
  * @author Stefan Wörner
  */
 public class HttpStatusCodeException extends RuntimeException
@@ -8,11 +10,20 @@ public class HttpStatusCodeException extends RuntimeException
 
 	private static final long serialVersionUID = 5536311157691373752L;
 
+	/**
+	 * Konstruktor.
+	 */
 	public HttpStatusCodeException()
 	{
 
 	}
 
+	/**
+	 * Konstruktor.
+	 * 
+	 * @param message
+	 *            Fehlermeldung
+	 */
 	public HttpStatusCodeException( String message )
 	{
 		super( message );
