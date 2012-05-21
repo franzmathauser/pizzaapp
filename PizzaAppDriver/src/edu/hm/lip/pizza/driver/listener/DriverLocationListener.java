18050be2d4e679a636917ab3a1fae7a9de727839
Stefan (nepo) <StNepomuk@web.de>
2012-05-21 21:34:04 +0200

    asynchronous server call + provider optimizations
    #27
    #37

--- PizzaAppDriver/src/edu/hm/lip/pizza/driver/listener/DriverLocationListener.java
+++ PizzaAppDriver/src/edu/hm/lip/pizza/driver/listener/DriverLocationListener.java
@@ -3,6 +3,7 @@ package edu.hm.lip.pizza.driver.listener;
 import com.google.android.maps.MapView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +13,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import edu.hm.lip.pizza.driver.AppConstants;
+import edu.hm.lip.pizza.driver.services.DriverLocationService;
 import edu.hm.lip.pizza.driver.util.location.LocationDrawer;
 
 /**
@@ -32,6 +34,8 @@ public class DriverLocationListener implements LocationListener
 	private static long m_lastLocationTime = Long.MAX_VALUE;
 
 	private static float m_lastLocationAccuracy = Float.MIN_VALUE;
+
+	private static boolean m_gpsProviderAvailable = true;
 
 	/**
 	 * Konstruktor.
@@ -60,44 +64,51 @@ public class DriverLocationListener implements LocationListener
 		Log.d( this.getClass().getSimpleName(), "location time: " + location.getTime() );
 		Log.d( this.getClass().getSimpleName(), "location accuracy: " + location.getAccuracy() );
 
-		// Wenn gespeicherter Positionszeitstempel und gespeicherte Positionsgenauigkeit nicht mehr den Anfangswert
-		// besitzen muss eine genauere Prüfung durchgeführt werden
-		if (m_lastLocationTime != Long.MAX_VALUE && m_lastLocationAccuracy != Float.MIN_VALUE)
+		// Wenn GPS Provider aktiviert ist UND auch verfügbar ist, dann muss genauer nachgeprüft werden ob das
+		// Positionsupdate verwendet werden soll oder nicht
+		if (m_locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) && m_gpsProviderAvailable)
 		{
-			// Wenn der neue Positionszeitstempel älter als der letzte verarbeitete Zeitstempel ist, dann
-			// Positionsänderung ignorieren
-			if (location.getTime() < m_lastLocationTime)
+			// Wenn gespeicherter Positionszeitstempel und gespeicherte Positionsgenauigkeit nicht mehr den Anfangswert
+			// besitzen muss eine genauere Prüfung durchgeführt werden
+			if (m_lastLocationTime != Long.MAX_VALUE && m_lastLocationAccuracy != Float.MIN_VALUE)
 			{
-				Log.d( this.getClass().getSimpleName(), "location time too old" );
-				return;
-			}
+				// Wenn der neue Positionszeitstempel älter als der letzte verarbeitete Zeitstempel ist, dann
+				// Positionsänderung ignorieren
+				if (location.getTime() < m_lastLocationTime)
+				{
+					Toast.makeText( m_context, "location time too old", Toast.LENGTH_SHORT ).show();
+					Log.d( this.getClass().getSimpleName(), "location time too old" );
+					return;
+				}
 
-			// Wenn der neue Positionszeitstempel abzüglich des Tolleranzbereichs älter als der letzte verarbeitete
-			// Zeitstempel ist und die neue Positionsgenauigkeit ungenauer ist, dann Positionsänderung ignorieren
-			if (location.getTime() - AppConstants.TIME_TOLERANCE < m_lastLocationTime
-					&& location.getAccuracy() > m_lastLocationAccuracy + AppConstants.ACCURACY_TOLERANCE)
-			{
-				Log.d( this.getClass().getSimpleName(), "location accuracy too inaccurate" );
-				return;
+				// Wenn der neue Positionszeitstempel abzüglich des Tolleranzbereichs älter als der letzte verarbeitete
+				// Zeitstempel ist und die neue Positionsgenauigkeit ungenauer ist, dann Positionsänderung ignorieren
+				if (location.getTime() - AppConstants.TIME_TOLERANCE < m_lastLocationTime
+						&& location.getAccuracy() > m_lastLocationAccuracy + AppConstants.ACCURACY_TOLERANCE)
+				{
+					Toast.makeText( m_context, "location accuracy too inaccurate", Toast.LENGTH_SHORT ).show();
+					Log.d( this.getClass().getSimpleName(), "location accuracy too inaccurate" );
+					return;
+				}
 			}
-		}
-		// Wenn gespeicherter Positionszeitstempel und gespeicherte Positionsgenauigkeit noch den Anfangswert
-		// besitzen muss eine genauere Prüfung durchgeführt werden
-		else if (m_lastLocationTime == Long.MAX_VALUE && m_lastLocationAccuracy == Float.MIN_VALUE)
-		{
-			// Letzte bekannte Position vom GPS Provider anfordern
-			Location lastGpsLocation = m_locationManager.getLastKnownLocation( LocationManager.GPS_PROVIDER );
-
-			// Wenn GPS Positionszeitstempel neuer ist als der neue Positionszeitstempel abzüglich der Toleranz,
-			// dann GPS Position verwenden
-			if (lastGpsLocation.getTime() > location.getTime() - AppConstants.TIME_TOLERANCE)
+			// Wenn gespeicherter Positionszeitstempel und gespeicherte Positionsgenauigkeit noch den Anfangswert
+			// besitzen muss eine genauere Prüfung durchgeführt werden
+			else if (m_lastLocationTime == Long.MAX_VALUE && m_lastLocationAccuracy == Float.MIN_VALUE)
 			{
-				location = lastGpsLocation;
-				Log.d( this.getClass().getSimpleName(), "using newer gps location for initial positioning" );
-				Log.d( this.getClass().getSimpleName(),
-						"gps location: " + lastGpsLocation.getLatitude() + "|" + lastGpsLocation.getLongitude() );
-				Log.d( this.getClass().getSimpleName(), "gps location time: " + lastGpsLocation.getTime() );
-				Log.d( this.getClass().getSimpleName(), "gps location accuracy: " + lastGpsLocation.getAccuracy() );
+				// Letzte bekannte Position vom GPS Provider anfordern
+				Location lastGpsLocation = m_locationManager.getLastKnownLocation( LocationManager.GPS_PROVIDER );
+
+				// Wenn GPS Positionszeitstempel neuer ist als der neue Positionszeitstempel abzüglich der Toleranz,
+				// dann GPS Position verwenden
+				if (lastGpsLocation.getTime() > location.getTime() - AppConstants.TIME_TOLERANCE)
+				{
+					location = lastGpsLocation;
+					Log.d( this.getClass().getSimpleName(), "using newer gps location for initial positioning" );
+					Log.d( this.getClass().getSimpleName(), "gps location: " + lastGpsLocation.getLatitude() + "|"
+							+ lastGpsLocation.getLongitude() );
+					Log.d( this.getClass().getSimpleName(), "gps location time: " + lastGpsLocation.getTime() );
+					Log.d( this.getClass().getSimpleName(), "gps location accuracy: " + lastGpsLocation.getAccuracy() );
+				}
 			}
 		}
 
@@ -106,6 +117,11 @@ public class DriverLocationListener implements LocationListener
 
 		// Neue Location zeichnen lassen
 		LocationDrawer.getInstance( m_context, m_mapView ).updateCurrentLocation( location );
+
+		Intent intent = new Intent( m_context, DriverLocationService.class );
+		intent.putExtra( DriverLocationService.EXTRA_LATITUDE, location.getLatitude() );
+		intent.putExtra( DriverLocationService.EXTRA_LONGITUDE, location.getLongitude() );
+		m_context.startService( intent );
 
 		// TODO Server Request asynchron -> Service
 		// try
@@ -161,17 +177,12 @@ public class DriverLocationListener implements LocationListener
 	@Override
 	public void onProviderDisabled( String provider )
 	{
-		// LocationProviderManager manager = LocationProviderManager.getInstance( m_context, m_mapView );
-		//
-		// // Wenn der deaktivierte Provider dem aktuell verwendeten Provider entspricht, dann muss deregistriert werden
-		// // und ggf. dem Benutzer ein Dialog angezeit werden, dass Provider für das Tracking benötigt werden
-		// if (!manager.getCurrentProvider().equals( provider ) && LocationManager.GPS_PROVIDER.equals( provider ))
-		// {
-		// manager.unregisterLocationListener();
-		// Log.d( this.getClass().getSimpleName(), provider + "disabled -> unregistered" );
-		// }
+		Log.d( this.getClass().getSimpleName(), provider + " provider disabled" );
 
-		Toast.makeText( m_context, provider + " provider disabled.", Toast.LENGTH_SHORT ).show();
+		if (LocationManager.GPS_PROVIDER.equals( provider ))
+		{
+			m_gpsProviderAvailable = false;
+		}
 	}
 
 	/**
@@ -182,17 +193,12 @@ public class DriverLocationListener implements LocationListener
 	@Override
 	public void onProviderEnabled( String provider )
 	{
-		// LocationProviderManager manager = LocationProviderManager.getInstance( m_context, m_mapView );
-		//
-		// // Wenn der aktivierte Provider nicht dem aktuell verwendeten Provider entspricht und der aktivierte Provider
-		// // der GPS Provider ist, dann auf den GPS Provider wechseln
-		// if (!manager.getCurrentProvider().equals( provider ) && LocationManager.GPS_PROVIDER.equals( provider ))
-		// {
-		// manager.switchProvider();
-		// Log.d( this.getClass().getSimpleName(), provider + "enabled -> using it now" );
-		// }
+		Log.d( this.getClass().getSimpleName(), provider + " provider enabled" );
 
-		Toast.makeText( m_context, provider + " provider enabled.", Toast.LENGTH_SHORT ).show();
+		if (LocationManager.GPS_PROVIDER.equals( provider ))
+		{
+			m_gpsProviderAvailable = true;
+		}
 	}
 
 	/**
@@ -208,15 +214,27 @@ public class DriverLocationListener implements LocationListener
 		switch (status)
 		{
 			case LocationProvider.OUT_OF_SERVICE:
-				Toast.makeText( m_context, provider + " provider is out of service.", Toast.LENGTH_SHORT ).show();
+				Log.d( this.getClass().getSimpleName(), provider + " provider is out of service" );
+				if (LocationManager.GPS_PROVIDER.equals( provider ))
+				{
+					m_gpsProviderAvailable = false;
+				}
 				break;
 
 			case LocationProvider.TEMPORARILY_UNAVAILABLE:
-				Toast.makeText( m_context, provider + " provider is temporarily unavailable.", Toast.LENGTH_SHORT ).show();
+				Log.d( this.getClass().getSimpleName(), provider + " provider is temporarily unavailable" );
+				if (LocationManager.GPS_PROVIDER.equals( provider ))
+				{
+					m_gpsProviderAvailable = false;
+				}
 				break;
 
 			case LocationProvider.AVAILABLE:
-				Toast.makeText( m_context, provider + " provider is available.", Toast.LENGTH_SHORT ).show();
+				Log.d( this.getClass().getSimpleName(), provider + " provider is available" );
+				if (LocationManager.GPS_PROVIDER.equals( provider ))
+				{
+					m_gpsProviderAvailable = true;
+				}
 				break;
 
 			default:
