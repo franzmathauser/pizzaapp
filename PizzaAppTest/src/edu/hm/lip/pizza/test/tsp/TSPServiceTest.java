453d97cb4d5aaa1a43850c230a2090db3266a64c
Franz Mathauser <franz.mathauser@googlemail.com>
2012-06-05 01:00:19 +0200

    added junit assertions for tsp-service close #83

--- PizzaAppTest/src/edu/hm/lip/pizza/test/tsp/TSPServiceTest.java
+++ PizzaAppTest/src/edu/hm/lip/pizza/test/tsp/TSPServiceTest.java
@@ -3,6 +3,8 @@ package edu.hm.lip.pizza.test.tsp;
 import java.util.ArrayList;
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import edu.hm.lip.pizza.test.AbstractTest;
@@ -36,24 +38,47 @@ public class TSPServiceTest extends AbstractTest
 	public void testTSP() throws Exception
 	{
 		List<Address> destinationList = new ArrayList<Address>();
-		destinationList.add( new Address( "Oberbiberger Str. 10 München" ) );
-		destinationList.add( new Address( "Adams-Lehmannstr. 16, München" ) );
-		destinationList.add( new Address( "Lothstr.64, München" ) );
-		destinationList.add( new Address( "Schatzelweg 12, Schliersee" ) );
-		destinationList.add( new Address( "Pestalozzistr. 50, München" ) );
-		destinationList.add( new Address( "Schloßhof 14, Seefeld" ) );
-		destinationList.add( new Address( "Tölzerstr. 1, Grünwald" ) );
+		Address address1 = new Address( "Oberbiberger Str. 10 München" );
+		Address address2 = new Address( "Adams-Lehmannstr. 16, München" );
+		Address address3 = new Address( "Lothstr.64, München" );
+		destinationList.add( address1 );
+		destinationList.add( address2 );
+		destinationList.add( address3 );
 
 		PermutationManager permManager = new RoundTripPermutation( destinationList );
 		List<Path> tspPaths = permManager.getPaths();
 
+		Assert.assertEquals( "Nummer of Possible Paths", 2, tspPaths.size() );
+		Assert.assertEquals( "Nummer Edges of Path 1", 4, tspPaths.get( 0 ).size() );
+		Assert.assertEquals( "Nummer Edges of Path 2", 4, tspPaths.get( 1 ).size() );
+
 		GoogleDistanceMatrix gDistanceMatrix = new DistanceMatrixBroker( destinationList ).requestDistanceMatrix();
+
 		MatrixContainer<Integer> matrix = new MatrixContainerAdapter( gDistanceMatrix, Measurements.DURATION ).getInstance();
 
-		System.out.println( matrix );
+		Assert.assertNotNull( "Google Distance Matrix", matrix );
+		Assert.assertNotNull( "Distance Matrix 0x0", matrix.get( 0, 0 ) );
+		Assert.assertNotNull( "Distance Matrix 1x0", matrix.get( 1, 0 ) );
+		Assert.assertNotNull( "Distance Matrix 2x0", matrix.get( 2, 0 ) );
+		Assert.assertNotNull( "Distance Matrix 0x1", matrix.get( 0, 1 ) );
+		Assert.assertNotNull( "Distance Matrix 1x1", matrix.get( 1, 1 ) );
+		Assert.assertNotNull( "Distance Matrix 2x1", matrix.get( 2, 1 ) );
+		Assert.assertNotNull( "Distance Matrix 0x2", matrix.get( 0, 2 ) );
+		Assert.assertNotNull( "Distance Matrix 1x2", matrix.get( 1, 2 ) );
+		Assert.assertNotNull( "Distance Matrix 2x2", matrix.get( 2, 2 ) );
 
 		TspSolver solver = new TspPermutation( matrix, tspPaths );
-		System.out.println( "\n" + solver.solve() );
+
+		Path tspPath = solver.solve();
+
+		Assert.assertEquals( "Nummer Edges im TSP-Path", 4, tspPath.getEdges().size() );
+		Assert.assertEquals( "Nummer Edges im TSP-Path", 4, tspPath.getEdges().size() );
+
+		Assert.assertEquals( address1, tspPath.getEdges().get( 0 ).getAddress() );
+		Assert.assertEquals( address3, tspPath.getEdges().get( 1 ).getAddress() );
+		Assert.assertEquals( address2, tspPath.getEdges().get( 2 ).getAddress() );
+		Assert.assertEquals( address1, tspPath.getEdges().get( 3 ).getAddress() );
+
 	}
 
 }
