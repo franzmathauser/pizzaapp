aacc2b39204f51f3b4d62c28d774f6349f234cc0
Franz Mathauser <franz.mathauser@googlemail.com>
2012-06-04 23:45:39 +0200

    fixed format
    added audio-tag for waiter-voice
    
    inserted ispeech service-trick
    
    close #81

--- PizzaAppServices/WebContent/bakery/js/ordertracking.js
+++ PizzaAppServices/WebContent/bakery/js/ordertracking.js
@@ -2,7 +2,8 @@
  * ActiveMQ MessageHandler bei eingehenden Bestellungen. Eine neu eingehende
  * Bestellung wird in die GUI eingefügt.
  * 
- * @param order Bestellungsobjekt
+ * @param order
+ *            Bestellungsobjekt
  */
 function ordersTopic(order) {
 
@@ -12,4 +13,29 @@ function ordersTopic(order) {
 	$('li[order_id|="' + order.id + '"]').click(function() {
 		orderLineClickHandler(order.id, $(this).attr('order_stage'));
 	});
+
+	playWaiterVoice(order);
+}
+
+function playWaiterVoice(order) {
+	var text = 'Neue Bestellung erhalten: \n';
+	for ( var i = 0; i < order.orderLines.length; i++) {
+		var orderLine = order.orderLines[i];
+		if (orderLine.quantity == '1') {
+			text += 'Ein mal';
+		} else {
+			text += orderLine.quantity + ' mal';
+		}
+		text += products[orderLine.productId].name + ' ';
+		text += orderLine.size + '. ..\n';
+	}
+
+	text = encodeURI(text);
+
+	$('#audio')
+			.attr(
+					'src',
+					'http://api.ispeech.org/api/rest?voice=eurgermanfemale&action=convert&text='
+							+ text
+							+ '&apikey=8d1e2e5d3909929860aede288d6b974e&speed=0');
 }
