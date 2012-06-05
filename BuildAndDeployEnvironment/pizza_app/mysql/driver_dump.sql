6ddd1246d175c3c6578474ad0ed4ec88b243384e
Franz Mathauser <franz.mathauser@googlemail.com>
2012-06-05 22:12:25 +0200

    mysql-dump der fahrer und produkte

---
+++ BuildAndDeployEnvironment/pizza_app/mysql/driver_dump.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM pizzaapp_db.driver
+LIMIT 0, 1000
+
+-- Date: 2012-06-05 22:10
+*/
+INSERT INTO `driver` (`id`,`name`) VALUES (1,'Franz');
+INSERT INTO `driver` (`id`,`name`) VALUES (2,'Stefan');
+INSERT INTO `driver` (`id`,`name`) VALUES (3,'Martin');
+INSERT INTO `driver` (`id`,`name`) VALUES (4,'Robert');
---
+++ BuildAndDeployEnvironment/pizza_app/mysql/product_dump.sql
@@ -0,0 +1,29 @@
+/*
+-- Query: SELECT * FROM pizzaapp_db.product
+LIMIT 0, 1000
+
+-- Date: 2012-06-05 22:07
+*/
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (4,'Tomatensoße, Käße, Salami','salami.jpg','Pizza Salami');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (5,'Pizzabrot','pane.jpg','Pizza Pane ');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (6,'Tomatensoße','marinara.jpg','Pizza Marinara');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (7,'Tomatensoße, Käse','margherita.jpg','Pizza Margherita');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (8,'Tomatensoße, Käse, Schinken','prociutto.jpg','Pizza Prociutto');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (9,'Tomatensoße, Käse, Schinken, Pilze','regina.jpg','Pizza Regina');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (10,'Tomatensoße, Käse, Pilze','funghi.jpg','Pizza Funghi');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (11,'Tomatensoße, Käse, Schinken, Pilze, Sardellen, Oliven','capricciosa.jpg','Pizza Capricciosa');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (12,'Tomatensoße, Käse, Schinken, Pilze
+geschlossen, Käsegratinierung','calzone.jpg','Pizza Calzone');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (13,'Tomatensoße, Käse, Artischocken, Pilze, Salami','qstag.jpg','Pizza Quattro Stagione');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (14,'Tomatensoße, Käse, Thunfisch, Zwiebeln','tecip.jpg','Pizza Tonno e Cipolla');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (15,'Tomatensoße, Käse, Meeresfrüchte','mare.jpg','Pizza Ai Frutti di Mare');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (16,'Tomatensoße, Käse, Garnelen, Cocktailtomaten','scampi.jpg','Pizza Scampi');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (17,'Tomatensoße, Käse, frische Tomaten, Schinken, Rucola','primavera.jpg','Pizza Primavera');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (18,'Tomantensoße, Käse, Parmaschinken, Parmesan, Rucola','parma.jpg','Pizza Parma');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (19,'Tomantensoße, Käse (Mozzarella, Parmesan, Provelone, Gorgonzola)','qformaggi.jpg','Pizza Quattro Formaggi');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (20,'Tomatensoße, Büffelmozzarella','buffalo.jpg','Pizza Buffalo');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (21,'Tomatensoße, Käse, kalabresische Wildschweinsalami','calabrese.jpg','Pizza Calabrese');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (22,'Tomatensoße, Käse, scharfe Salami, Peperoni','diavola.jpg','Pizza Diavola');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (23,'Tomatensoße, Käse, Schinken, Ananas','hawaii.jpg','Pizza Hawaii');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (24,'mit Allem was das Herz wünscht','speciale.jpg','Pizza Speciale');
+INSERT INTO `product` (`id`,`description`,`imageUrl`,`name`) VALUES (25,'Ei, Sauerrahm, Speck, Zwiebeln','flammk.jpg','Pizza Flammkuchen');
