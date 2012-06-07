73e4cf118aaf2e0f21044fcdb2091e30270cc4c0
Stefan (nepo) <StNepomuk@web.de>
2012-06-07 18:29:23 +0200

    added service extra constans
    #42
    #27
    #41

---
+++ PizzaAppDriver/src/edu/hm/lip/pizza/driver/service/extra/ExtraConstants.java
@@ -0,0 +1,44 @@
+package edu.hm.lip.pizza.driver.service.extra;
+
+/**
+ * Konstenten für Intent, Broadcast und Service Extras.
+ * 
+ * @author Stefan Wörner
+ */
+public final class ExtraConstants
+{
+
+	/**
+	 * Privater Konstruktor.
+	 */
+	private ExtraConstants()
+	{
+
+	}
+
+	/**
+	 * Extra-Konstante für das Successful-Flag.
+	 */
+	public static final String SUCCESSFUL_EXTRA = "successful";
+
+	/**
+	 * Extra-Konstante für das Refresh-Flag.
+	 */
+	public static final String REFRESH_EXTRA = "refresh";
+
+	/**
+	 * Extra-Konstante für Fehlermeldungen.
+	 */
+	public static final String ERROR_MSG_EXTRA = "error_message";
+
+	/**
+	 * Extra-Konstante für die Latitude der aktuellen Position.
+	 */
+	public static final String EXTRA_LATITUDE = "lat";
+
+	/**
+	 * Extra-Konstante für die Longitude der aktuellen Position.
+	 */
+	public static final String EXTRA_LONGITUDE = "lon";
+
+}
