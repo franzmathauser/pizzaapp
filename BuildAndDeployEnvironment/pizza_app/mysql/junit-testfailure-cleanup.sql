45d6c085ad1fe1aecfd3a59b5d27ff476574f9fa
Franz Mathauser <franz.mathauser@googlemail.com>
2012-06-14 10:12:56 +0200

    junit-testfailure-cleanupscript
    #98

---
+++ BuildAndDeployEnvironment/pizza_app/mysql/junit-testfailure-cleanup.sql
@@ -0,0 +1,14 @@
+DELETE from gps_data WHERE driver_id > 4;
+
+DELETE from driver WHERE id > 4;
+
+DELETE from order_line WHERE productConfiguration_id IN (SELECT id from product_configuration WHERE product_id > 30);
+
+DELETE from order_stage WHERE id IN (SELECT order_id from order_line WHERE productConfiguration_id IN (SELECT id from product_configuration WHERE product_id > 30));
+
+DELETE from order_table WHERE id IN (SELECT order_id from order_line WHERE productConfiguration_id IN (SELECT id from product_configuration WHERE product_id > 30));
+
+DELETE from product_configuration WHERE product_id > 30;
+
+DELETE from product WHERE id > 30;
+
