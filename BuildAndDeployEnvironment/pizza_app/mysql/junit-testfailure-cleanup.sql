e599fa9fd341d018e37df3b602bcb5f796313162
Franz Mathauser <franz.mathauser@googlemail.com>
2012-06-14 11:05:53 +0200

    added more generic where clauses #98

--- BuildAndDeployEnvironment/pizza_app/mysql/junit-testfailure-cleanup.sql
+++ BuildAndDeployEnvironment/pizza_app/mysql/junit-testfailure-cleanup.sql
@@ -1,25 +1,33 @@
 -- löschen aller GPS-Daten von Testfahrern.
 DELETE from gps_data 
- WHERE driver_id > 4;
+ WHERE driver_id IN ( SELECT id from driver 
+                       WHERE name LIKE 'TestDriver%'
+                    );
 
 -- löschen aller Testfahrer
 DELETE from driver 
- WHERE id > 4;
+ WHERE name LIKE 'TestDriver%';
 
 -- löschen aller Bestellposten, von Testkunden
 DELETE from order_line 
- WHERE productConfiguration_id IN ( SELECT id 
-                                      from product_configuration 
-                                    WHERE product_id > 30
+ WHERE productConfiguration_id IN ( SELECT id from product_configuration 
+                                    WHERE product_id IN (SELECT id from product 
+                                                          WHERE description LIKE 'TestDescription%'
+                                                            AND imageUrl LIKE 'testURL%'
+                                                            AND name LIKE 'testName%'
+                                                        )
                                   );
 
 -- löschen aller Bestellphasen, von Testprodukten
 DELETE from order_stage 
  WHERE id IN ( SELECT order_id 
                  from order_line 
-                WHERE productConfiguration_id IN ( SELECT id 
-                                                     from product_configuration 
-                                                    WHERE product_id > 30
+                WHERE productConfiguration_id IN ( SELECT id from product_configuration 
+                                                    WHERE product_id IN ( SELECT id from product 
+                                                                          WHERE description LIKE 'TestDescription%'
+                                                                            AND imageUrl LIKE 'testURL%'
+                                                                            AND name LIKE 'testName%'
+                                                                         )
                                                  )
              );
 
@@ -27,19 +35,28 @@ DELETE from order_stage
 DELETE from order_table 
  WHERE id IN ( SELECT order_id 
                  from order_line 
-                WHERE productConfiguration_id IN ( SELECT id 
-                                                     from product_configuration 
-                                                    WHERE product_id > 30
+                WHERE productConfiguration_id IN ( SELECT id from product_configuration 
+                                                    WHERE product_id IN ( SELECT id from product 
+                                                                          WHERE description LIKE 'TestDescription%'
+                                                                            AND imageUrl LIKE 'testURL%'
+                                                                            AND name LIKE 'testName%'
+                                                                         )
                                                  )
              );
 
 -- löschen aller Produktekonfiguration, die nicht initial angelegt wurden.
 DELETE from product_configuration 
- WHERE product_id > 30;
+ WHERE product_id IN (SELECT id from product 
+                       WHERE description LIKE 'TestDescription%'
+                         AND imageUrl LIKE 'testURL%'
+                         AND name LIKE 'testName%'
+                     );
 
 -- löschen aller Produkte, die nicht initial angelegt wurden.
 DELETE from product 
- WHERE id > 30;
+ WHERE description LIKE 'TestDescription%'
+   AND imageUrl LIKE 'testURL%'
+   AND name LIKE 'testName%';
 
 -- löschen aller order stages von Testkunden
 DELETE FROM order_stage WHERE order_id IN (
@@ -66,6 +83,3 @@ DELETE from customer
        WHERE company LIKE 'Test%' 
          AND department LIKE 'TestDepartment%' 
         AND level LIKE 'TestLevel%';
-
-
-
